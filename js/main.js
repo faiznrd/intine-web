63f7ac7772e10992db11f7d22ebc4429c3bfaf89
faiznrd <milkocak23@gmail.com>
2021-06-29 21:11:59 +0700

    nambahi minuman

--- js/main.js
+++ js/main.js
@@ -122,8 +122,27 @@ const cookieMiddleware = () => {
             $('[lang="en"]').hide();
         }
     }else{
-        $('[lang="en"]').hide();
+        $('[lang="id"]').hide();
     }
+}
+function nextSlide(){
+    let activeSlide = document.querySelector('.slide.translate-x-0');
+    activeSlide.classList.remove('translate-x-0');
+    activeSlide.classList.add('-translate-x-full');
+    
+    let nextSlide = activeSlide.nextElementSibling;
+    nextSlide.classList.remove('translate-x-full');
+    nextSlide.classList.add('translate-x-0');
+}
+
+function previousSlide(){
+    let activeSlide = document.querySelector('.slide.translate-x-0');
+    activeSlide.classList.remove('translate-x-0');
+    activeSlide.classList.add('translate-x-full');
+    
+    let previousSlide = activeSlide.previousElementSibling;
+    previousSlide.classList.remove('-translate-x-full');
+    previousSlide.classList.add('translate-x-0');
 }
 Sammy('#main', function() {
     this.get('#/home', function() { 
