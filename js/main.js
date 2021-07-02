8151c6d0061cc274e9b94a8694bac70f15435b5a
faiznrd <milkocak23@gmail.com>
2021-07-02 17:25:52 +0700

    anu

--- js/main.js
+++ js/main.js
@@ -74,6 +74,37 @@ $(window).scroll(function() {
         $(".navbar").removeClass("bg-darker-red")
     }
 })
+const beritaMiddleware = () => {
+    $("#berita-top").append(                    
+        '<div class="flex">'+
+    '<div class="w-2/5 h-full w-full rounded-3xl black-shadow overflow-hidden">'+
+        '<div class="thumbnail h-96 bg-gray-900 overflow-hidden"><img class="w-full" src="assets/img/wisata/kota/malioboro.png"></div>'+
+        '<div class="thumbnail p-9 bg-white">'+
+        '<div class="text-sm text-gray-400 font-light pb-6"><img class="inline-block pr-3" src="assets/svg/date-icon.svg" alt="" srcset="">20 Juni 2009</div>'+
+        '<div class="text-base font-bold pb-6">Pengunjung Malioboro Diwajibkan Mematuhi Protokol Kesehatan Guna Menekan Angka Persebaran COVID-19</div>'+
+        '<div class="text-sm font-light text-red-500 pt-5 font-bold"><a href="#">Baca Selengkapnya</a></div>'+
+    '</div>'+
+   '</div>'+
+   '<div class="w-3/5 flex flex-col pl-5">'+
+   '<div class="pb-5"><div class="h-72 w-full flex rounded-3xl black-shadow overflow-hidden">'+
+       '<div class="thumbnail w-2/5 bg-gray-900 overflow-hidden"><img class="h-full max-w-none" src="assets/img/andhong.png"></div>'+
+       '<div class="thumbnail h-auto w-3/5 p-9 bg-white"><div class="text-sm text-gray-400 font-light pb-6"><img class="inline-block pr-3" src="assets/svg/date-icon.svg" alt="" srcset="">13 Juni 1999</div>'+
+           '<div class="text-base font-bold">Delman atau yang Lebih Dikenal dengan Nama Andhong Menjadi Daya Tarik Tersendiri Bagi Para Wisatawan yang Mengunjungi Malioboro</div>'+
+           '<div class="text-sm font-light text-red-500 pt-5 font-bold"><a href="#">Baca Selengkapnya</a></div>'+
+       '</div>'+
+   '</div></div>'+
+   '<div class=""><div class="h-72 w-full rounded-3xl flex black-shadow overflow-hidden">'+
+       '<div class="thumbnail w-2/5 bg-gray-900 overflow-hidden"><img class="h-full max-w-none" src="assets/img/bakpia.png"></div>'+
+       '<div class="thumbnail h-auto w-3/5 p-9 bg-white">'+
+           '<div class="text-sm text-gray-400 font-light pb-6"><img class="inline-block pr-3" src="assets/svg/date-icon.svg" alt="" srcset="">19 Augstus 1969</div>'+
+           '<div class="text-base font-bold">Bakpia Pathok dengan Cita Rasa yang Khas Menjadi Pilihan Wisatawan Sebagai Oleh-Oleh yang Wajib Dibeli Ketika Berlibur ke Jogja</div>'+
+           '<div class="text-sm font-light text-red-500 pt-5 font-bold"><a href="#">Baca Selengkapnya</a></div>'+
+       '</div>'+
+   '</div></div>'+
+'</div>'+
+   '</div>')
+
+}
 $(".open-modal").click(function(){
     console.log(1)
     var modal_id = $(this).data("modal-id")
@@ -83,6 +114,7 @@ $(".button-close").click(() => {$(".modal").hide()})
 var app = Sammy('#main', function() {
     this.get("/", function(){
         $("#main").load("home.html", () => {
+            beritaMiddleware()
             AOS.init();
             slider()
             $(".open-modal").click(function(){
@@ -91,39 +123,11 @@ var app = Sammy('#main', function() {
                 $("#"+modal_id).show()
             })
             $(".button-close").click(() => {$(".modal").hide()})
-            $("#berita-top").append(                    
-                '<div class="flex">'+
-            '<div class="w-2/5 h-full w-full rounded-3xl black-shadow overflow-hidden">'+
-                '<div class="thumbnail h-96 bg-gray-900 overflow-hidden"><img class="w-full" src="assets/img/wisata/kota/malioboro.png"></div>'+
-                '<div class="thumbnail p-9 bg-white">'+
-                '<div class="text-sm text-gray-400 font-light pb-6"><img class="inline-block pr-3" src="assets/svg/date-icon.svg" alt="" srcset="">20 Juni 2009</div>'+
-                '<div class="text-base font-bold pb-6">Pengunjung Malioboro Diwajibkan Mematuhi Protokol Kesehatan Guna Menekan Angka Persebaran COVID-19</div>'+
-                '<div class="text-sm font-light text-red-500 pt-5 font-bold"><a href="#">Baca Selengkapnya</a></div>'+
-            '</div>'+
-           '</div>'+
-           '<div class="w-3/5 flex flex-col pl-5">'+
-           '<div class="pb-5"><div class="h-72 w-full flex rounded-3xl black-shadow overflow-hidden">'+
-               '<div class="thumbnail w-2/5 bg-gray-900 overflow-hidden"><img class="h-full max-w-none" src="assets/img/andhong.png"></div>'+
-               '<div class="thumbnail h-auto w-3/5 p-9 bg-white"><div class="text-sm text-gray-400 font-light pb-6"><img class="inline-block pr-3" src="assets/svg/date-icon.svg" alt="" srcset="">13 Juni 1999</div>'+
-                   '<div class="text-base font-bold">Delman atau yang Lebih Dikenal dengan Nama Andhong Menjadi Daya Tarik Tersendiri Bagi Para Wisatawan yang Mengunjungi Malioboro</div>'+
-                   '<div class="text-sm font-light text-red-500 pt-5 font-bold"><a href="#">Baca Selengkapnya</a></div>'+
-               '</div>'+
-           '</div></div>'+
-           '<div class=""><div class="h-72 w-full rounded-3xl flex black-shadow overflow-hidden">'+
-               '<div class="thumbnail w-2/5 bg-gray-900 overflow-hidden"><img class="h-full max-w-none" src="assets/img/bakpia.png"></div>'+
-               '<div class="thumbnail h-auto w-3/5 p-9 bg-white">'+
-                   '<div class="text-sm text-gray-400 font-light pb-6"><img class="inline-block pr-3" src="assets/svg/date-icon.svg" alt="" srcset="">19 Augstus 1969</div>'+
-                   '<div class="text-base font-bold">Bakpia Pathok dengan Cita Rasa yang Khas Menjadi Pilihan Wisatawan Sebagai Oleh-Oleh yang Wajib Dibeli Ketika Berlibur ke Jogja</div>'+
-                   '<div class="text-sm font-light text-red-500 pt-5 font-bold"><a href="#">Baca Selengkapnya</a></div>'+
-               '</div>'+
-           '</div></div>'+
-        '</div>'+
-           '</div>')
-
         })
     })
     this.get('#/', function() {
         $("#main").load("home.html", () => {
+            beritaMiddleware()
             AOS.init();
             slider()
             $(".open-modal").click(function(){
@@ -132,40 +136,6 @@ var app = Sammy('#main', function() {
                 $("#"+modal_id).show()
             })
             $(".button-close").click(() => {$(".modal").hide()})
-            $.ajax({
-                url:"https://gnews.io/api/v4/search?q=yogyakarta&token=e985cc7144bc55b1551dbc46c1fb0ce2&lang=en"
-            }).then((data) =>{
-                console.log(data)
-                $("#berita-top").append(                    
-                '<div class="grid grid-cols-2 gap-4">'+
-            '<div class="h-full w-full rounded-3xl">'+
-                '<div class="thumbnail h-96 bg-gray-900 overflow-hidden"><img class="w-full" src="'+data.articles[2]["image"]+'"></div>'+
-                '<div class="thumbnail p-9 bg-white">'+
-                '<div class="text-sm text-gray-400 font-light pb-6"><img class="inline-block pr-3" src="assets/svg/date-icon.svg" alt="" srcset="">'+data.articles[2]["publishedAt"]+'</div>'+
-                '<div class="text-base font-bold pb-6">'+data.articles[2]["title"]+'</div>'+
-                '<div class="text-sm font-light">'+data.articles[2]["description"]+'</div>'+
-                '<div class="text-sm font-light text-red-500 pt-5 font-bold"><a href="'+data.articles[2]["url"]+'">Baca Selengkapnya</a></div>'+
-            '</div>'+
-           '</div>'+
-           '<div class="grid grid-cols-1 gap-4">'+
-           '<div class="h-full w-full rounded-3xl">'+
-               '<div class="thumbnail h-60 bg-gray-900 overflow-hidden"><img class="h-full" src="'+data.articles[3]["image"]+'"></div>'+
-               '<div class="thumbnail h-auto p-9 bg-white"><div class="text-sm text-gray-400 font-light pb-6"><img class="inline-block pr-3" src="assets/svg/date-icon.svg" alt="" srcset="">'+data.articles[3]["publishedAt"]+'</div>'+
-                   '<div class="text-base font-bold">'+data.articles[3]["title"]+'</div>'+
-                   '<div class="text-sm font-light text-red-500 pt-5 font-bold"><a href="'+data.articles[3]["url"]+'">Baca Selengkapnya</a></div>'+
-               '</div>'+
-           '</div>'+
-           '<div class="h-full w-full rounded-3xl">'+
-               '<div class="thumbnail h-60 bg-gray-900 overflow-hidden"><img class="h-full" src="'+data.articles[1]["image"]+'"></div>'+
-               '<div class="thumbnail h-auto p-9 bg-white">'+
-                   '<div class="text-sm text-gray-400 font-light pb-6"><img class="inline-block pr-3" src="assets/svg/date-icon.svg" alt="" srcset="">'+data.articles[1]["publishedAt"]+'</div>'+
-                   '<div class="text-base font-bold">'+data.articles[1]["title"]+'</div>'+
-                   '<div class="text-sm font-light text-red-500 pt-5 font-bold"><a href="'+data.articles[1]["url"]+'">Baca Selengkapnya</a></div>'+
-               '</div>'+
-           '</div>'+
-   '</div>'+
-           '</div>')
-            })
         })
     });
     this.get('#/explore/jogja', function() {
@@ -276,68 +246,9 @@ var app = Sammy('#main', function() {
         $("#main").load("berita.html", () => {
             $(".modal").hide()
             dropDownFunc()
-            $.ajax({
-                url:"https://gnews.io/api/v4/search?q=yogyakarta&token=e985cc7144bc55b1551dbc46c1fb0ce2&lang=en"
-            }).then((data) =>{
-                console.log(data)
-                $("#berita-top").append(
-                    '<div class="grid grid-cols-2 gap-4"><div class="grid grid-cols-1 gap-4">'+
-                '<div class="h-full w-full rounded-3xl">'+
-                '<div class="thumbnail h-60 bg-gray-900 overflow-hidden"><img class="h-full" src="'+data.articles[3]["image"]+'"></div>'+
-                    '<div class="thumbnail h-auto p-9 bg-white"><div class="text-sm text-gray-400 font-light pb-6"><img class="inline-block pr-3" src="assets/svg/date-icon.svg" alt="" srcset="">'+data.articles[3]["publishedAt"]+'</div>'+
-                        '<div class="text-base font-bold">'+data.articles[3]["title"]+'</div>'+
-                        '<div class="text-sm font-light text-red-500 pt-5 font-bold"><a href="'+data.articles[3]["url"]+'">Baca Selengkapnya</a></div>'+
-                    '</div>'+
-                '</div>'+
-                '<div class="h-full w-full rounded-3xl">'+
-                '<div class="thumbnail h-60 bg-gray-900 overflow-hidden"><img class="h-full" src="'+data.articles[1]["image"]+'"></div>'+
-                    '<div class="thumbnail h-auto p-9 bg-white">'+
-                        '<div class="text-sm text-gray-400 font-light pb-6"><img class="inline-block pr-3" src="assets/svg/date-icon.svg" alt="" srcset="">'+data.articles[1]["publishedAt"]+'</div>'+
-                        '<div class="text-base font-bold">'+data.articles[1]["title"]+'</div>'+
-                        '<div class="text-sm font-light text-red-500 pt-5 font-bold"><a href="'+data.articles[1]["url"]+'">Baca Selengkapnya</a></div>'+
-                    '</div>'+
-                '</div>'+
-            '</div>'+
-            '<div class="h-full w-full rounded-3xl">'+
-                '<div class="thumbnail h-96 bg-gray-900 overflow-hidden"><img class="w-full" src="'+data.articles[2]["image"]+'"></div>'+
-                '<div class="thumbnail h-auto p-9 bg-white">'+
-                '<div class="text-sm text-gray-400 font-light pb-6"><img class="inline-block pr-3" src="assets/svg/date-icon.svg" alt="" srcset="">'+data.articles[2]["publishedAt"]+'</div>'+
-                '<div class="text-base font-bold pb-6">'+data.articles[2]["title"]+'</div>'+
-                '<div class="text-sm font-light">'+data.articles[2]["description"]+'</div>'+
-                '<div class="text-sm font-light text-red-500 pt-5 font-bold"><a href="'+data.articles[2]["url"]+'">Baca Selengkapnya</a></div>'+
-            '</div>'+
-           '</div></div>'+
-           '<div class="grid grid-cols-3 gap-4 pt-5">'+
-           '<div class="h-full w-full rounded-3xl">'+
-           '<div class="thumbnail h-60 bg-gray-900 overflow-hidden"><img class="h-full" src="'+data.articles[4]["image"]+'"></div>'+
-               '<div class="thumbnail h-auto p-9 bg-white">'+
-                   '<div class="text-sm text-gray-400 font-light pb-6"><img class="inline-block pr-3" src="assets/svg/date-icon.svg" alt="" srcset="">'+data.articles[4]["publishedAt"]+'</div>'+
-                   '<div class="text-base font-bold">'+data.articles[4]["title"]+'</div>'+
-                   '<div class="text-sm font-light text-red-500 pt-5 font-bold"><a href="'+data.articles[4]["url"]+'">Baca Selengkapnya</a></div>'+
-               '</div>'+
-           '</div>'+
-           '<div class="h-full w-full rounded-3xl">'+
-           '<div class="thumbnail h-60 bg-gray-900 overflow-hidden"><img class="h-full" src="'+data.articles[5]["image"]+'"></div>'+
-               '<div class="thumbnail h-auto p-9 bg-white">'+
-                   '<div class="text-sm text-gray-400 font-light pb-6"><img class="inline-block pr-3" src="assets/svg/date-icon.svg" alt="" srcset="">'+data.articles[5]["publishedAt"]+'</div>'+
-                   '<div class="text-base font-bold">'+data.articles[5]["title"]+'</div>'+
-                   '<div class="text-sm font-light text-red-500 pt-5 font-bold"><a href="'+data.articles[5]["url"]+'">Baca Selengkapnya</a></div>'+
-               '</div>'+
-           '</div>'+
-           '<div class="h-full w-full rounded-3xl">'+
-           '<div class="thumbnail h-60 bg-gray-900 overflow-hidden"><img class="h-full" src="'+data.articles[6]["image"]+'"></div>'+
-               '<div class="thumbnail h-auto p-9 bg-white">'+
-                   '<div class="text-sm text-gray-400 font-light pb-6"><img class="inline-block pr-3" src="assets/svg/date-icon.svg" alt="" srcset="">'+data.articles[6]["publishedAt"]+'</div>'+
-                   '<div class="text-base font-bold">'+data.articles[6]["title"]+'</div>'+
-                   '<div class="text-sm font-light text-red-500 pt-5 font-bold"><a href="'+data.articles[6]["url"]+'">Baca Selengkapnya</a></div>'+
-               '</div>'+
-           '</div>'+
-           +'</div>'
-           )
-            })
-        });
-    });
-  }).run();
+        })
+    })
+    }).run();
 function dropDownFunc(){
     $("#dropdown-navigator").on('change', function() {
         console.log(this.value)
