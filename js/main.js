12f7bf5ebc8ad62fe1af01d241fe7e4bfc2d1b4a
faiznrd <milkocak23@gmail.com>
2021-07-02 15:16:35 +0700

    add berita

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(".modal").hide()
+
 function slider(){
     var default_slide = $("#card-slider").children()[0]
     var default_progress = $("#slider-progress").children()[0]
@@ -50,6 +51,11 @@ const activeMenu = (id) => {
     $(id).addClass("border-b-2")
     $(id).addClass("border-orange")
 }
+$("#bahasa").hide()
+$("#bahasa-click").click(() =>{
+    $("#bahasa").toggle()
+    console.log(0000000)
+})
 $(window).scroll(function() {
     var navbarHeight = $(".navbar").height()
     console.log($(this).scrollTop())
@@ -58,14 +64,14 @@ $(window).scroll(function() {
         $(".navbar").removeClass("absolute")
         $(".navbar").removeClass("bg-gradient-linear")
         $(".navbar").addClass("fixed")
-        $(".navbar").addClass("bg-red-jogja")
+        $(".navbar").addClass("bg-darker-red")
         $(".navbar").addClass("black-shadow")
     }else{
         $(".navbar").removeClass("black-shadow")
         $(".navbar").addClass("absolute")
         $(".navbar").addClass("bg-gradient-linear")
         $(".navbar").removeClass("fixed")
-        $(".navbar").removeClass("bg-red-jogja")
+        $(".navbar").removeClass("bg-darker-red")
     }
 })
 $(".open-modal").click(function(){
@@ -87,9 +93,9 @@ var app = Sammy('#main', function() {
             $(".button-close").click(() => {$(".modal").hide()})
             $("#berita-top").append(                    
                 '<div class="grid grid-cols-2 gap-4">'+
-            '<div class="h-full w-full rounded-3xl">'+
+            '<div class="h-full w-full rounded-3xl black-shadow overflow-hidden">'+
                 '<div class="thumbnail h-96 bg-gray-900 overflow-hidden"><img class="w-full" src="assets/img/wisata/kota/malioboro.png"></div>'+
-                '<div class="thumbnail p-9 bg-white black-shadow">'+
+                '<div class="thumbnail p-9 bg-white">'+
                 '<div class="text-sm text-gray-400 font-light pb-6"><img class="inline-block pr-3" src="assets/svg/date-icon.svg" alt="" srcset="">20 Juni 2009</div>'+
                 '<div class="text-base font-bold pb-6">Pengunjung Malioboro Diwajibkan Mematuhi Protokol Kesehatan Guna Menekan Angka Persebaran COVID-19</div>'+
                 '<div class="text-sm font-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh nibh eu id sit sit sed mi. Pellentesque sit nulla fames volutpat eu, mi. Lobortis lorem cursus odio volutpat non pellentesque risus. Et egestas elit vitae orci commodo mollis adipiscing nisl arcu. Eu cras vulputate facilisis pretium eu fermentum in vel. Pellentesque ante cras risus, dapibus ultricies cum suscipit.</div>'+
@@ -97,16 +103,16 @@ var app = Sammy('#main', function() {
             '</div>'+
            '</div>'+
            '<div class="grid grid-cols-1 gap-4">'+
-           '<div class="h-full w-full rounded-3xl">'+
+           '<div class="h-full w-full rounded-3xl black-shadow overflow-hidden">'+
                '<div class="thumbnail h-60 bg-gray-900 overflow-hidden"><img class="w-full" src="assets/img/andhong.png"></div>'+
-               '<div class="thumbnail h-auto p-9 bg-white black-shadow"><div class="text-sm text-gray-400 font-light pb-6"><img class="inline-block pr-3" src="assets/svg/date-icon.svg" alt="" srcset="">13 Juni 1999</div>'+
+               '<div class="thumbnail h-auto p-9 bg-white"><div class="text-sm text-gray-400 font-light pb-6"><img class="inline-block pr-3" src="assets/svg/date-icon.svg" alt="" srcset="">13 Juni 1999</div>'+
                    '<div class="text-base font-bold">Delman atau yang Lebih Dikenal dengan Nama Andhong Menjadi Daya Tarik Tersendiri Bagi Para Wisatawan yang Mengunjungi Malioboro</div>'+
                    '<div class="text-sm font-light text-red-500 pt-5 font-bold"><a href="#">Baca Selengkapnya</a></div>'+
                '</div>'+
            '</div>'+
-           '<div class="h-full w-full rounded-3xl">'+
+           '<div class="h-full w-full rounded-3xl black-shadow overflow-hidden">'+
                '<div class="thumbnail h-60 bg-gray-900 overflow-hidden"><img class="w-full" src="assets/img/bakpia.png"></div>'+
-               '<div class="thumbnail h-auto p-9 bg-white black-shadow">'+
+               '<div class="thumbnail h-auto p-9 bg-white">'+
                    '<div class="text-sm text-gray-400 font-light pb-6"><img class="inline-block pr-3" src="assets/svg/date-icon.svg" alt="" srcset="">19 Augstus 1969</div>'+
                    '<div class="text-base font-bold">Bakpia Pathok dengan Cita Rasa yang Khas Menjadi Pilihan Wisatawan Sebagai Oleh-Oleh yang Wajib Dibeli Ketika Berlibur ke Jogja</div>'+
                    '<div class="text-sm font-light text-red-500 pt-5 font-bold"><a href="#">Baca Selengkapnya</a></div>'+
@@ -135,7 +141,7 @@ var app = Sammy('#main', function() {
                 '<div class="grid grid-cols-2 gap-4">'+
             '<div class="h-full w-full rounded-3xl">'+
                 '<div class="thumbnail h-96 bg-gray-900 overflow-hidden"><img class="w-full" src="'+data.articles[2]["image"]+'"></div>'+
-                '<div class="thumbnail p-9 bg-white black-shadow">'+
+                '<div class="thumbnail p-9 bg-white">'+
                 '<div class="text-sm text-gray-400 font-light pb-6"><img class="inline-block pr-3" src="assets/svg/date-icon.svg" alt="" srcset="">'+data.articles[2]["publishedAt"]+'</div>'+
                 '<div class="text-base font-bold pb-6">'+data.articles[2]["title"]+'</div>'+
                 '<div class="text-sm font-light">'+data.articles[2]["description"]+'</div>'+
@@ -145,14 +151,14 @@ var app = Sammy('#main', function() {
            '<div class="grid grid-cols-1 gap-4">'+
            '<div class="h-full w-full rounded-3xl">'+
                '<div class="thumbnail h-60 bg-gray-900 overflow-hidden"><img class="h-full" src="'+data.articles[3]["image"]+'"></div>'+
-               '<div class="thumbnail h-auto p-9 bg-white black-shadow"><div class="text-sm text-gray-400 font-light pb-6"><img class="inline-block pr-3" src="assets/svg/date-icon.svg" alt="" srcset="">'+data.articles[3]["publishedAt"]+'</div>'+
+               '<div class="thumbnail h-auto p-9 bg-white"><div class="text-sm text-gray-400 font-light pb-6"><img class="inline-block pr-3" src="assets/svg/date-icon.svg" alt="" srcset="">'+data.articles[3]["publishedAt"]+'</div>'+
                    '<div class="text-base font-bold">'+data.articles[3]["title"]+'</div>'+
                    '<div class="text-sm font-light text-red-500 pt-5 font-bold"><a href="'+data.articles[3]["url"]+'">Baca Selengkapnya</a></div>'+
                '</div>'+
            '</div>'+
            '<div class="h-full w-full rounded-3xl">'+
                '<div class="thumbnail h-60 bg-gray-900 overflow-hidden"><img class="h-full" src="'+data.articles[1]["image"]+'"></div>'+
-               '<div class="thumbnail h-auto p-9 bg-white black-shadow">'+
+               '<div class="thumbnail h-auto p-9 bg-white">'+
                    '<div class="text-sm text-gray-400 font-light pb-6"><img class="inline-block pr-3" src="assets/svg/date-icon.svg" alt="" srcset="">'+data.articles[1]["publishedAt"]+'</div>'+
                    '<div class="text-base font-bold">'+data.articles[1]["title"]+'</div>'+
                    '<div class="text-sm font-light text-red-500 pt-5 font-bold"><a href="'+data.articles[1]["url"]+'">Baca Selengkapnya</a></div>'+
@@ -271,14 +277,14 @@ var app = Sammy('#main', function() {
                     '<div class="grid grid-cols-2 gap-4"><div class="grid grid-cols-1 gap-4">'+
                 '<div class="h-full w-full rounded-3xl">'+
                 '<div class="thumbnail h-60 bg-gray-900 overflow-hidden"><img class="h-full" src="'+data.articles[3]["image"]+'"></div>'+
-                    '<div class="thumbnail h-auto p-9 bg-white black-shadow"><div class="text-sm text-gray-400 font-light pb-6"><img class="inline-block pr-3" src="assets/svg/date-icon.svg" alt="" srcset="">'+data.articles[3]["publishedAt"]+'</div>'+
+                    '<div class="thumbnail h-auto p-9 bg-white"><div class="text-sm text-gray-400 font-light pb-6"><img class="inline-block pr-3" src="assets/svg/date-icon.svg" alt="" srcset="">'+data.articles[3]["publishedAt"]+'</div>'+
                         '<div class="text-base font-bold">'+data.articles[3]["title"]+'</div>'+
                         '<div class="text-sm font-light text-red-500 pt-5 font-bold"><a href="'+data.articles[3]["url"]+'">Baca Selengkapnya</a></div>'+
                     '</div>'+
                 '</div>'+
                 '<div class="h-full w-full rounded-3xl">'+
                 '<div class="thumbnail h-60 bg-gray-900 overflow-hidden"><img class="h-full" src="'+data.articles[1]["image"]+'"></div>'+
-                    '<div class="thumbnail h-auto p-9 bg-white black-shadow">'+
+                    '<div class="thumbnail h-auto p-9 bg-white">'+
                         '<div class="text-sm text-gray-400 font-light pb-6"><img class="inline-block pr-3" src="assets/svg/date-icon.svg" alt="" srcset="">'+data.articles[1]["publishedAt"]+'</div>'+
                         '<div class="text-base font-bold">'+data.articles[1]["title"]+'</div>'+
                         '<div class="text-sm font-light text-red-500 pt-5 font-bold"><a href="'+data.articles[1]["url"]+'">Baca Selengkapnya</a></div>'+
@@ -287,7 +293,7 @@ var app = Sammy('#main', function() {
             '</div>'+
             '<div class="h-full w-full rounded-3xl">'+
                 '<div class="thumbnail h-96 bg-gray-900 overflow-hidden"><img class="w-full" src="'+data.articles[2]["image"]+'"></div>'+
-                '<div class="thumbnail h-auto p-9 bg-white black-shadow">'+
+                '<div class="thumbnail h-auto p-9 bg-white">'+
                 '<div class="text-sm text-gray-400 font-light pb-6"><img class="inline-block pr-3" src="assets/svg/date-icon.svg" alt="" srcset="">'+data.articles[2]["publishedAt"]+'</div>'+
                 '<div class="text-base font-bold pb-6">'+data.articles[2]["title"]+'</div>'+
                 '<div class="text-sm font-light">'+data.articles[2]["description"]+'</div>'+
@@ -297,7 +303,7 @@ var app = Sammy('#main', function() {
            '<div class="grid grid-cols-3 gap-4 pt-5">'+
            '<div class="h-full w-full rounded-3xl">'+
            '<div class="thumbnail h-60 bg-gray-900 overflow-hidden"><img class="h-full" src="'+data.articles[4]["image"]+'"></div>'+
-               '<div class="thumbnail h-auto p-9 bg-white black-shadow">'+
+               '<div class="thumbnail h-auto p-9 bg-white">'+
                    '<div class="text-sm text-gray-400 font-light pb-6"><img class="inline-block pr-3" src="assets/svg/date-icon.svg" alt="" srcset="">'+data.articles[4]["publishedAt"]+'</div>'+
                    '<div class="text-base font-bold">'+data.articles[4]["title"]+'</div>'+
                    '<div class="text-sm font-light text-red-500 pt-5 font-bold"><a href="'+data.articles[4]["url"]+'">Baca Selengkapnya</a></div>'+
@@ -305,7 +311,7 @@ var app = Sammy('#main', function() {
            '</div>'+
            '<div class="h-full w-full rounded-3xl">'+
            '<div class="thumbnail h-60 bg-gray-900 overflow-hidden"><img class="h-full" src="'+data.articles[5]["image"]+'"></div>'+
-               '<div class="thumbnail h-auto p-9 bg-white black-shadow">'+
+               '<div class="thumbnail h-auto p-9 bg-white">'+
                    '<div class="text-sm text-gray-400 font-light pb-6"><img class="inline-block pr-3" src="assets/svg/date-icon.svg" alt="" srcset="">'+data.articles[5]["publishedAt"]+'</div>'+
                    '<div class="text-base font-bold">'+data.articles[5]["title"]+'</div>'+
                    '<div class="text-sm font-light text-red-500 pt-5 font-bold"><a href="'+data.articles[5]["url"]+'">Baca Selengkapnya</a></div>'+
@@ -313,7 +319,7 @@ var app = Sammy('#main', function() {
            '</div>'+
            '<div class="h-full w-full rounded-3xl">'+
            '<div class="thumbnail h-60 bg-gray-900 overflow-hidden"><img class="h-full" src="'+data.articles[6]["image"]+'"></div>'+
-               '<div class="thumbnail h-auto p-9 bg-white black-shadow">'+
+               '<div class="thumbnail h-auto p-9 bg-white">'+
                    '<div class="text-sm text-gray-400 font-light pb-6"><img class="inline-block pr-3" src="assets/svg/date-icon.svg" alt="" srcset="">'+data.articles[6]["publishedAt"]+'</div>'+
                    '<div class="text-base font-bold">'+data.articles[6]["title"]+'</div>'+
                    '<div class="text-sm font-light text-red-500 pt-5 font-bold"><a href="'+data.articles[6]["url"]+'">Baca Selengkapnya</a></div>'+
